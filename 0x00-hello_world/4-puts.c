1525584f6b2e6905d6f8b1a9da4402b0bda57878
Aduragbemi Oduntan <aduragbemioduntan@gmail.com>
2022-06-30 19:09:09 -0700

    Hot fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
 /* A simple output code using puts */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
 
