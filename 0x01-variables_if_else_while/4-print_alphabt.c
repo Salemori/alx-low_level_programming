ec1b5ec13a5f971b835e4acb81edf3b0147c13a8
Aduragbemi Oduntan <aduragbemioduntan@gmail.com>
2022-07-04 21:39:53 +0000

    Correction C variable 4 and 9

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -11,8 +11,7 @@ int main(void)
 	char alp;
 
 	for (alp = 'a' ; alp <= 'z' ; alp++)
-		putchar(alp);
-	if (alp != 'e' && alp != 'q')
+		if (alp != 'e' && alp != 'q')
 		putchar(alp);
 	putchar('\n');
 	return (0);
